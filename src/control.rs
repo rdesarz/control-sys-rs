ef554132c26fc9d41ce7d02495f128a5f025a80f
Romain Desarzens <hib.pub@protonmail.com>
2025-01-26 15:57:44 +0000

    Add documentation of modules (#4)

--- src/control.rs
+++ src/control.rs
@@ -1,7 +1,12 @@
-/*
 
-*/
+/// Methods to analyze control systems
 pub mod analysis;
+
+/// Structures to represent control systems
 pub mod model;
+
+/// Methods to simulate control systems in time-space
 pub mod simulator;
+
+/// Methods to generate trajectories 
 pub mod trajectory;
